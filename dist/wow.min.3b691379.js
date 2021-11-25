5e7ccab943103aa701eaa63769587123b43eb913
ganeshbadhe5 <ganesh.badhe5@gmail.com>
2021-11-25 11:45:07 +0530

    added new products

--- dist/wow.min.3b691379.js
+++ dist/wow.min.3b691379.js
@@ -497,7 +497,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52452" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49509" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
